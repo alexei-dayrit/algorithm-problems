7374532c4d47167b702e5e8fd79a05d8b7dc9fec
Alexei Dayrit <alexeigdayrit@gmail.com>
2022-05-10 15:25:30 -0700

    Completed best time to buy and sell stock on LeetCode.

---
+++ best-time-to-buy-and-sell-stock/main.js
@@ -0,0 +1,29 @@
+var maxProfit = function (prices) {
+  let profit = 0;
+  let lowest = prices[0];
+  for (let i = 0; i < prices.length; i++) {
+    let potentialProfit = prices[a] - prices[i];
+    if (prices[i] < lowest) {
+      lowest = prices[i];
+    }
+    if (potentialProfit > profit) {
+      profit = potentialProfit;
+    }
+  }
+  return profit;
+};
+
+/* BRUTE FORCE METHOD
+var maxProfit = function (prices) {
+  let profit = 0;
+  for (let i = 0; i < prices.length; i++) {
+    for (let a = i + 1; a < prices.length; a++) {
+      let potentialProfit = prices[a] - prices[i];
+      if (potentialProfit > profit) {
+        profit = potentialProfit;
+      }
+    }
+  }
+  return profit;
+};
+*/
