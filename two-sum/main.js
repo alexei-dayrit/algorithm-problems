3cca99d550fd56c355f32c45b60c4ba1637c87ce
Alexei Dayrit <alexeigdayrit@gmail.com>
2022-10-02 16:53:01 -0700

    Added explanation for 1 pass solution.

--- two-sum/main.js
+++ two-sum/main.js
@@ -1,3 +1,19 @@
+/* 1 pass solution explanation
+instead of finding what array elements add to target, we search for current elem - target
+dont need to initialize hashmap, so we don't have to deal with duplicates check
+
+- create obj
+- loop through array
+- if target - array[i] is in obj then return [obj[target-array[i]], i]
+- else add the array elem as the prop and it's index into the obj
+
+ex:
+const indices = {
+    value: index
+}
+*/
+
+
 var twoSum = function (nums, target) {
   const obj = {};
   for (let i = 0; i < nums.length; i++) {
@@ -8,3 +24,19 @@ var twoSum = function (nums, target) {
     obj[neededNum] = i;
   }
 };
+
+
+/* Brute force method
+
+var twoSum = function(nums, target) {
+    const indices = []
+    for (let i = 0; i < nums.length; i++) {
+        for (let j = i + 1; j < nums.length; j++) {
+            if ((nums[i] + nums[j]) === target) {
+                indices.push(i, j)
+            }
+        }
+    }
+    return indices
+};
+*/
