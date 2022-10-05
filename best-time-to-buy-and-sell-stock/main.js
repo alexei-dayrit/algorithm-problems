8304470177c73bab25e6b11815d1af89b3a20209
Alexei Dayrit <alexeigdayrit@gmail.com>
2022-10-05 00:29:57 +0000

    Added solution and explanation with ppointers for best-time-to-buy-and-sell-stocks.

--- best-time-to-buy-and-sell-stock/main.js
+++ best-time-to-buy-and-sell-stock/main.js
@@ -1,3 +1,33 @@
+/* 1 pass solution with pointers
+- const bestProfit = 0
+- const left = 0 (buy date)
+- const right = 1 (sell date)
+- while right pointer > prices.length
+  - if prices at left < prices at right
+    - then there is a profit so, profit = right - left
+    - compare if better than bestProfit, so bestProfit = Math.max(profit, bestProfit)
+  - else left = right pointer because right pointer is the new min price
+  - right++ at end to shift sell date and find best profit
+- return profit
+
+*/
+
+var maxProfit = function (prices) {
+  let [bestProfit, left, right] = [0, 0, 1];
+
+  while (right < prices.length) {
+    if (prices[left] < prices[right]) {
+      const profit = prices[right] - prices[left];
+      bestProfit = Math.max(bestProfit, profit);
+    } else {
+      left = right;
+    }
+    right++;
+  }
+  return bestProfit;
+};
+
+/*
 var maxProfit = function (prices) {
   let profit = 0;
   let lowest = prices[0];
@@ -6,7 +36,7 @@ var maxProfit = function (prices) {
       lowest = prices[i];
     }
 
-    let potentialProfit = prices[i] - lowest;
+    const potentialProfit = prices[i] - lowest;
 
     if (potentialProfit > profit) {
       profit = potentialProfit;
@@ -14,6 +44,7 @@ var maxProfit = function (prices) {
   }
   return profit;
 };
+*/
 
 /* BRUTE FORCE METHOD
 var maxProfit = function (prices) {
