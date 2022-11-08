54b5b19e1d55bce28156de0cd04309e6bb736f45
Alexei Dayrit <alexeigdayrit@gmail.com>
2022-11-07 21:01:42 -0800

    Updated function to  handle when test cases are capitalized.

--- check-if-substring/main.js
+++ check-if-substring/main.js
@@ -1,10 +1,16 @@
 // Function checks whether string1 has substring (string2) in it
 const isSubstring = (string1, string2) => {
-  console.log('🚀 ~ file: main.js ~ line 4 ~ isSubstring ~ s1.includes(string2);', s1.includes(string2));
-  return s1.includes(string2);
+  const loweredS1 = string1.toLowerCase();
+  const loweredS2 = string2.toLowerCase();
+  console.log('🚀 ~ file: main.js ~ line 4 ~ isSubstring ~ s1.includes(string2);', loweredS1.includes(loweredS2));
+  return loweredS1.includes(loweredS2);
 };
 
-const s1 = 'san francisco';
-const s2 = 'fran';
+const test1 = 'san francisco';
+const test2 = 'fran';
 
-console.log('return:', isSubstring(s1, s2));
+const test3 = 'san FrAncisco';
+const test4 = 'fran';
+
+console.log('return:', isSubstring(test1, test2));
+console.log('return:', isSubstring(test3, test4));
